91984aef015bef84230abae38890bbd663dd0c0b
arun kumar <arun.devpro@gmail.com>
2024-10-12 11:30:11 +0530

    footer styles and markup

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,11 +1,74 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from "react-icons/fa";
+import "../styles/Footer.css";
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className="footer">
       <div className="footer-container">
-        <h1>Hotel.com</h1>
-        <span> Copyright © 1996–2024 Hotel.com. All rights reserved.</span>
+        <div className="footer-section">
+          <h4>Company</h4>
+          <ul className="footer-list">
+            <li>
+              <Link to="/about">About Us</Link>
+            </li>
+            <li>
+              <Link to="/careers">Careers</Link>
+            </li>
+            <li>
+              <Link to="/blog">Blog</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="footer-section">
+          <h4>Support</h4>
+          <ul className="footer-list">
+            <li>
+              <Link to="/help">Help Center</Link>
+            </li>
+            <li>
+              <Link to="/faq">FAQs</Link>
+            </li>
+            <li>
+              <Link to="/terms">Terms of Service</Link>
+            </li>
+            <li>
+              <Link to="/privacy">Privacy Policy</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="footer-section">
+          <h4>Follow Us</h4>
+          <ul className="footer-socials">
+            <li>
+              <a href="https://facebook.com">
+                <FaFacebook /> Facebook
+              </a>
+            </li>
+            <li>
+              <a href="https://twitter.com">
+                <FaTwitter /> Twitter
+              </a>
+            </li>
+            <li>
+              <a href="https://instagram.com">
+                <FaInstagram /> Instagram
+              </a>
+            </li>
+            <li>
+              <a href="https://linkedin.com">
+                <FaLinkedin /> LinkedIn
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="footer-bottom">
+        <p>&copy; 2024 Your Company. All rights reserved.</p>
       </div>
     </footer>
   );
